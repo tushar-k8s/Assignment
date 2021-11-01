ac57ad9208b2a38afd516a57d2fe6a16966202c5
tushar-k8s <93354961+tushar-k8s@users.noreply.github.com>
2021-11-02 03:29:17 +0530

    log analytics workspace id added

--- logging.tf
+++ logging.tf
@@ -19,44 +19,12 @@ resource "azurerm_log_analytics_solution" "azure_logsolution" {
 }
 
 
-resource "azurerm_eventhub_namespace" "logging" {
-  name                = "logging-eventhub"
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = azurerm_resource_group.RG.name
-  sku                 = "Standard"
-  capacity            = 1
-  kafka_enabled       = false
-}
-
-
-resource "azurerm_eventhub" "logging_aks" {
-  name                = "logging-aks-eventhub"
-  namespace_name      = azurerm_eventhub_namespace.logging.name
-  resource_group_name = azurerm_resource_group.RG.name
-  partition_count     = 2
-  message_retention   = 1
-}
-
-
-resource "azurerm_eventhub_namespace_authorization_rule" "logging-rule" {
-  name                = "authorization_rule"
-  namespace_name      = azurerm_eventhub_namespace.logging.name
-  resource_group_name = azurerm_resource_group.RG.name
-  
-
-  listen = true
-  send   = true
-  manage = true
-}
-
 
 
 resource "azurerm_monitor_diagnostic_setting" "aks-logging" {
   name                           = "diagnostic_aksl"
   target_resource_id             = azurerm_kubernetes_cluster.aks_cluster.id
-  eventhub_name                  = azurerm_eventhub.logging_aks.name
-  eventhub_authorization_rule_id = azurerm_eventhub_namespace_authorization_rule.logging-rule.id
-  #log_analytics_workspace_id     = azurerm_log_analytics_workspace.azure_workspace.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.azure_workspace.id
 
   log {
     category = "kube-scheduler"
